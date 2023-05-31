f08bfd8067ff6f977bef39f82d6c3336fe3d641d
codingrecipe1 <codingrecipe1@gmail.com>
2023-05-31 16:33:02 +0900

    Initial commit

---
+++ src/main/java/com/icia/member/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.icia.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+
+
+}
---
+++ src/main/java/com/icia/member/controller/MemberController.java
@@ -0,0 +1,38 @@
+package com.icia.member.controller;
+
+import com.icia.member.dto.MemberDTO;
+import com.icia.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/save")
+    public String saveForm() {
+        return "memberPages/memberSave";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute MemberDTO memberDTO) {
+        memberService.save(memberDTO);
+        return "index";
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        model.addAttribute("memberList", memberDTOList);
+        return "memberPages/memberList";
+    }
+}
