d040c8ded1f4a93a739a250948179fad8423c4fb
codingrecipe1 <codingrecipe1@gmail.com>
2023-06-05 15:25:27 +0900

    06_인터셉터 적용

--- src/main/java/com/icia/member/controller/MemberController.java
+++ src/main/java/com/icia/member/controller/MemberController.java
@@ -31,22 +31,34 @@ public class MemberController {
 
     @GetMapping("/")
     public String findAll(Model model) {
+        System.out.println("MemberController.findAll");
         List<MemberDTO> memberDTOList = memberService.findAll();
         model.addAttribute("memberList", memberDTOList);
         return "memberPages/memberList";
     }
 
     @GetMapping("/login")
-    public String loginForm() {
+    public String loginForm(@RequestParam(value = "redirectURI", defaultValue = "/member/mypage") String redirectURI,
+                            Model model) {
+        System.out.println("MemberController.loginForm");
+        System.out.println("redirectURI = " + redirectURI);
+        model.addAttribute("redirectURI", redirectURI);
         return "memberPages/memberLogin";
     }
 
     @PostMapping("/login")
-    public String memberLogin(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+    public String memberLogin(@ModelAttribute MemberDTO memberDTO, HttpSession session,
+                              @RequestParam("redirectURI") String redirectURI) {
+        System.out.println("MemberController.memberLogin");
+        System.out.println("URI" + redirectURI);
         boolean loginResult = memberService.login(memberDTO);
         if (loginResult) {
             session.setAttribute("loginEmail", memberDTO.getMemberEmail());
-            return "memberPages/memberMain";
+//            return "memberPages/memberMain";
+            // 로그인 성공하면 사용자가 직전에 요청한 주소로 redirect
+            // 인터셉터에 걸리지 않고 처음부터 로그인하는 사용자였다면
+            // redirect:/member/mypage 로 요청되며, memberMain 화면으로 전환됨.
+            return "redirect:" + redirectURI;
         } else {
             return "memberPages/memberLogin";
         }
@@ -96,6 +108,24 @@ public class MemberController {
         MemberDTO memberDTO = memberService.findById(id);
         model.addAttribute("member", memberDTO);
         return "memberPages/memberDetail";
+    }
+
+    @PostMapping("/dup-check")
+    public ResponseEntity emailCheck(@RequestBody MemberDTO memberDTO) {
+//        memberService.findByMemberEmail(memberDTO.getMemberEmail());
+//        return new ResponseEntity<>(HttpStatus.OK);
+        boolean result = memberService.emailCheck(memberDTO.getMemberEmail());
+        if (result) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "index";
     }
 
 }
